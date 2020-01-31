e7c113548fdb2b8860f891a7656686cf2a706499
krumilurfigur <gabrieloberg3@gmail.com>
2020-01-31 11:15:36 +0100

    lektionen 2020-01-31

--- src/komplex.java
+++ src/komplex.java
@@ -1,2 +1,13 @@
 public class komplex {
+    private double a;
+    private double b;
+
+    public komplex(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
 }
+
+    public static void main(String[] args) {
+
+    }
