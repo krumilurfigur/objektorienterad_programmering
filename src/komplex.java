97d54f4ee267425be8c174ec520c03abc1c91c61
krumilurfigur <gabrieloberg3@gmail.com>
2020-02-06 11:15:30 +0100

    lektionen 2020-02-06

--- src/komplex.java
+++ src/komplex.java
@@ -6,8 +6,28 @@ public class komplex {
         this.a = a;
         this.b = b;
     }
-}
+
+    @Override
+    public String toString() {
+        return a + " + " + b + "i";
+    }
 
     public static void main(String[] args) {
-
+        komplex a = new komplex(3, 2);
+        komplex b = new komplex(4, 1);
+        komplex c = add(a,b);
+        System.out.println(c);
+        komplex d = multiply(a,b);
     }
+
+    private static komplex add(komplex a, komplex b) {
+        komplex c = new komplex(a.a + b.a, a.b + b.b);
+        return c;
+    }
+
+    private static komplex multiply(komplex a, komplex b) {
+
+        return null;
+    }
+
+}
